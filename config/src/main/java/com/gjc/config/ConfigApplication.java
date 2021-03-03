5bbc1850cd40d861f83446e4cf72b05dc6f267f4
Gjc <15168264328@163.com>
2021-03-03 22:25:25 +0800

    增加本地文件的使用配置

--- config/src/main/java/com/gjc/config/ConfigApplication.java
+++ config/src/main/java/com/gjc/config/ConfigApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableConfigServer
+@EnableConfigServer  // 启动配置中心
 public class ConfigApplication {
 
     public static void main(String[] args) {
--- config_client/src/main/java/com/gjc/config_client/ConfigClientApplication.java
+++ config_client/src/main/java/com/gjc/config_client/ConfigClientApplication.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableEurekaClient
 @RestController
 public class ConfigClientApplication {
 
@@ -16,6 +17,8 @@ public class ConfigClientApplication {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
+
+    // 访问 localhost:8881/hi -》 foo version 21 获取到config-client-dev文件上的foo值
     @Value("${foo}")
     String foo;
     @RequestMapping(value = "/hi")
