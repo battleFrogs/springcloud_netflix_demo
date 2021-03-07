920a6a7222c250c4fb023c0cdbd880a6fe43674f
Gjc <15168264328@163.com>
2021-03-07 13:55:50 +0800

    增加gateway的服务注册

---
+++ gateway_register/src/main/java/com/gjc/gateway_register/GatewayRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.gjc.gateway_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayRegisterApplication.class, args);
+    }
+
+}
---
+++ gateway_register/src/test/java/com/gjc/gateway_register/GatewayRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjc.gateway_register;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
